9ceb7c9a3cce09816e380dfc0921b07f519ea216
Ujwal211096 <123488114+Ujwal211096@users.noreply.github.com>
2023-11-26 06:22:34 +0000

    com

--- .devcontainer/02_Basic/02_arrays.js
+++ .devcontainer/02_Basic/02_arrays.js
@@ -8,5 +8,13 @@ const myHeroes = marvel_heroes.concat(dc_heroes)//it returns new array
 
 //console.log(myHeroes);
 
+const all_heroes = [...marvel_heroes, ...dc_heroes]
+console.log(all_heroes);
 
+const another_array = [1, 2, 3, [4, 5, 6], 7, [8, 9, [10, 11]]]
+
+ let real_another_array = another_array.flat(Infinity);
+ console.log(real_another_array);
+
+//console.log(Array.isArray("Superman"));
 
