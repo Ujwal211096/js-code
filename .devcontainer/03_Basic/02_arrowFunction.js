78436fbcd468fa2017ca22dcc5096f64b62038fa
Ujwal211096 <123488114+Ujwal211096@users.noreply.github.com>
2023-11-29 06:05:57 +0000

    commit

---
+++ .devcontainer/03_Basic/02_arrowFunction.js
@@ -0,0 +1,12 @@
+// const one = function addOne (num1, num2){
+//     return num1 + num2;
+// } //explicit return
+
+//const two = addTwo = () => (nuum1 + num2); //implicit return
+// const addTwo = ()=>{
+//   return num1 + num2;
+// } //explicit return
+
+// const addThree = (num1, num2, num3) =>( num1 + num2+ num3)//implicit return
+
+const addTwo = (num1, num2) =>({username : "Ujwal"});
---
+++ .devcontainer/03_Basic/04_iife.js
@@ -0,0 +1,12 @@
+//Immidiately Invoked Function Expression(IIFE)
+
+// (function chai(){
+//     console.log("DB CONNECTED");
+// })
+// //to avoid global  scope pollution we use iife
+// //where function is wrapped in () and from () we call the function
+// ();
+
+// (
+//     (name)=> (console.log(`name is ${name}`))
+// ) ("ujwal");
